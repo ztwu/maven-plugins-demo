0ba7d2ca29caaeea830714a7f06c0557c1e6d614
吴彰涛 <1182517419@qq.com>
2019-07-18 11:01:57 +0800

    Initial commit

---
+++ src/main/java/com/iflytek/Test.java
@@ -0,0 +1,13 @@
+package com.iflytek;
+
+/**
+ * created with idea
+ * user:ztwu
+ * date:2019/3/25
+ * description
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.print("nihao");
+    }
+}
